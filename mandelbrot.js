a759910ca36e8d3657a24e27a17db12749d2db34
evanhackett <evanwhackett@gmail.com>
2015-05-17 21:09:30 -0700

    set up html, css, and js file.

---
+++ mandelbrot.js
@@ -0,0 +1,9 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var resolution = canvas.width;
+// var width = canvas.width;
+// var height = canvas.height;
+
+// defines the max number of times we iterate for a given number
+var MAX_ITERATIONS = 200;
+
